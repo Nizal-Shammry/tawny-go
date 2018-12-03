3e1ab249f2f38b847559495893dba2cc819d6d5a
Nizal Alshammry (PGR) <b0910147@14compsci153.ncl.ac.uk>
2018-12-03 15:05:11 +0000

    Add project.clj
    depends on Tawny-2.0.1

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject tawny-go "1.0.0-SNAPSHOT"
+  :description "tawny version of GO"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                [uk.org.russet/tawny-owl "2.0.1-SNAPSHOT"]]
+  :main ^:skip-aot tawny-go.core
+  :jvm-opts ["-Xmx1000m"])
